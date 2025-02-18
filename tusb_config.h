587b8d009c96927e5d0c31733b06840a00fcd832
0xDEADBEEF <highimportant@protonmail.com>
2025-02-18 23:28:03 +0300

    support files

---
+++ tusb_config.h
@@ -0,0 +1,10 @@
+#ifndef _TUSB_CONFIG_H_
+#define _TUSB_CONFIG_H_
+
+#define CFG_TUD_CDC 1
+#define CFG_TUD_HID 1
+#define CFG_TUD_MSC 0
+#define CFG_TUD_MIDI 0
+#define CFG_TUD_CUSTOM_CLASS 0
+
+#endif
